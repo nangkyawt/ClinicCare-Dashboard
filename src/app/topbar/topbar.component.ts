9d7512d261b536f8bb95cbb42f7a6095516f7a04
nangkyawt <nangkyawthsuhlaing@gmail.com>
2024-11-06 21:29:19 +0630

    toogle

--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
-  styleUrls: ['./topbar.component.css']
+  styleUrls: ['./topbar.component.css'],
 })
 export class TopbarComponent {
+  isSearchVisible = true;
 
+  toggleSearch() {
+    this.isSearchVisible = !this.isSearchVisible;
+  }
 }
